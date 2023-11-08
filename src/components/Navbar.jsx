a3e82c6e30dc5e821bea0086192aaab4b259421f
Excelemma <excelemma6@gmail.com>
2023-11-08 11:46:36 +0000

    made some screen size changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
           <p>PORTFOLIO 2023</p>
         </div>
         <div className="navLink">
-          <a href="">RESUME</a>
+          <a href="https://docs.google.com/document/d/1q-FxMgcOwzgUBmgmXOwO7O-3EJJZV9Y6cGPpyhtXecM/edit?usp=sharing">RESUME</a>
           <a href="#works">WORKS</a>
           <a href="#about">ABOUT</a>
           <a href="#contact">CONTACT</a>
